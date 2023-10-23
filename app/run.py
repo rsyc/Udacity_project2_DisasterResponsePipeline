293e270943e578c8e3f1d5655c519ba7aced55f4
root <rojan.saghian@gmail.com>
2023-10-23 20:11:27 +0000

    run.py is modified

--- app/run.py
+++ app/run.py
@@ -1,36 +1,122 @@
+import re
 import json
 import plotly
 import pandas as pd
 
+import nltk
+nltk.download(['punkt', 'wordnet', 'stopwords'])
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
 
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
+from sklearn.base import BaseEstimator, TransformerMixin
+
 
 
 app = Flask(__name__)
 
 def tokenize(text):
-    tokens = word_tokenize(text)
+    '''
+    This function is for tokenizing a given text 
+    (eg. each message from X in the model). Steps include
+    tokenizing sentences into words, lemmatizing (words root), 
+    normalizing (all to lower case), removing stop words and empty spaces
+    
+    Input:
+        text: a given text (a message at a time)
+    output:
+        clean_tokens: cleaned tokens to be used in the model        
+    '''
+    # tokenize text
+    words = word_tokenize(text)
+    
+    # initiate lemmatizer
     lemmatizer = WordNetLemmatizer()
 
+    # iterate through each token
     clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
+    for tok in words:
+        # lemmatize, normalize case, and remove leading/trailing white space
+        tok = tok.lower()
+        clean_tok = re.sub(r'[^a-zA-Z0-9]', ' ', lemmatizer.lemmatize(tok, pos='v')).strip()
+        # remove stop words and empty strings
+        if clean_tok != '' and clean_tok not in stopwords.words("english"):
+            clean_tokens.append(clean_tok)
+     
     return clean_tokens
 
+    
+    #tokens = word_tokenize(text)
+    #lemmatizer = WordNetLemmatizer()
+
+    #clean_tokens = []
+    #for tok in tokens:
+    #    clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+    #    clean_tokens.append(clean_tok)
+
+    #return clean_tokens
+
+class GenreExtractor(BaseEstimator, TransformerMixin):
+    '''
+    This is a custom Transfer function to separate genre
+    data from the text data
+    '''
+    def get_genre_data(self, x):
+        '''
+        This function separates genre data and turn the one column
+        of genre (in text) to columns of dummies each representing
+        one gerne. These columns then will be used in the model for
+        training and test.
+        '''
+        genres = [record[1] for record in x]
+        genre_dummies = pd.get_dummies(genres)
+        return genre_dummies
+   
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        # apply function to all values in X
+        X_tagged = self.get_genre_data(X) 
+
+        return pd.DataFrame(X_tagged)
+
+class TextExtractor(BaseEstimator, TransformerMixin):
+    '''
+    This is a custom Transfer function to separate text
+    data from the genre data
+    '''
+    def get_text_data(self, x):
+        '''
+        this function separates the text data and returns
+        it to be used in the model for further analysis.
+        '''
+        return [record[0] for record in x]
+      
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        # apply function to all values in X
+        X_tagged = self.get_text_data(X) 
+
+        return X_tagged
+
+
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///data/DisasterResponse.db')
+df = pd.read_sql_table('processedData', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("./models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
